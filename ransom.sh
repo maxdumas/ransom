28303d25605387f30d519e00cd75ce4e908db1d7
Maximillian Dumas <maxdumas@amazon.com>
2020-04-08 19:38:59 -0400

    Small cleanup

--- ransom.sh
+++ ransom.sh
@@ -1,5 +1,7 @@
 #!/bin/zsh
 
+set -euo pipefail
+
 TEXT="$@"
 CHAR_HEIGHT=64
 CHAR_WIDTH=64
@@ -9,15 +11,17 @@ colors=($(convert -list Color | awk '{print $1}'))
 
 for i in {1..$#TEXT}; do
     c=$TEXT[i]
-    echo $i $c
     if [ $c = ' ' ]; then
         convert -size ${CHAR_WIDTH}x${CHAR_HEIGHT} xc:white "__$i.png"
     else
         font="$fonts[$[$RANDOM%$#fonts+1]]"
-        fgcolor=$colors[$[$RANDOM%$#colors+1]]
+        fgcolor="$colors[$[$RANDOM%$#colors+1]]"
+        bgcolor="transparent"
         rotation=$[$RANDOM%30-15]
-        convert -size ${CHAR_WIDTH}x${CHAR_HEIGHT} -rotate $rotation -background transparent -gravity center -fill "$fgcolor" -font "$font" label:"$c" "__$i.png"
+        convert -size ${CHAR_WIDTH}x${CHAR_HEIGHT} -rotate $rotation -background "$bgcolor" -gravity center -fill "$fgcolor" -font "$font" label:"$c" "__$i.png"
     fi
 done
 
 convert __{1..$#TEXT}.png -colorspace SRGB -set page "+%[fx:$CHAR_WIDTH*t]+0" -background white -layers merge +repage output.png
+
+rm -f __*.png
